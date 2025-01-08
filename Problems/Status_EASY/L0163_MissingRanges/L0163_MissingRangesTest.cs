08d86518b6739137f90b1a2e238d89f84a47ea6c
Jorge Varela <jcvarela@live.com>
2025-01-08 11:36:30 -0500

    fix test cases for problem 0163 Missing Ranges

--- Problems/Status_EASY/L0163_MissingRanges/L0163_MissingRangesTest.cs
+++ Problems/Status_EASY/L0163_MissingRanges/L0163_MissingRangesTest.cs
@@ -5,12 +5,17 @@ namespace LeetCode_Problems.Status_EASY.L0163_MissingRanges
 {
     public class L0163_MissingRangesTest
     {
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            yield return new object[] { new int[] { }, 0, 99, new int[][] { new int[] { 0, 99 } } };
+            yield return new object[] { new int[] { 0, 1, 3, 50, 75 }, 0, 99, new int[][] { new int[] { 2, 2 }, new int[] { 4, 49 }, new int[] { 51, 74 }, new int[] { 76, 99 } } };
+            yield return new object[] { new int[] { 1, 3, 50, 75 }, 0, 99, new int[][] { new int[] { 0, 0 }, new int[] { 2, 2 }, new int[] { 4, 49 }, new int[] { 51, 74 }, new int[] { 76, 99 } } };
+            yield return new object[] { new int[] { 0, 1, 3, 50, 75 }, 0, 75, new int[][] { new int[] { 2, 2 }, new int[] { 4, 49 }, new int[] { 51, 74 } } };
+        }
+
         [Theory]
-        [InlineData(new int[] { }, 0, 99, new int[][] { new int[] { 0, 99 } })]
-        [InlineData(new int[] { 0, 1, 3, 50, 75 }, 0, 99, new int[][] { new int[] { 2, 2 }, new int[] { 4, 49 }, new int[] { 51, 74 }, new int[] { 76, 99 } })]
-        [InlineData(new int[] { 1, 3, 50, 75 }, 0, 99, new int[][] { new int[] { 0, 0 }, new int[] { 2, 2 }, new int[] { 4, 49 }, new int[] { 51, 74 }, new int[] { 76, 99 } })]
-        [InlineData(new int[] { 0, 1, 3, 50, 75 }, 0, 75, new int[][] { new int[] { 2, 2 }, new int[] { 4, 49 }, new int[] { 51, 74 } })]
-        [InlineData(new int[] { 0, 1, 3, 50, 75 }, 0, 74, new int[][] { new int[] { 2, 2 }, new int[] { 4, 49 }, new int[] { 51, 74 } })]
+        [MemberData(nameof(GetTestCases))]
         public void FindMissingRanges_Test(int[] nums, int lower, int upper, int[][] expected)
         {
             var result = MissingRanges.FindMissingRanges(nums, lower, upper);
